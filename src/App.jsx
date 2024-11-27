f99ecb94d604814d60e9fb5d63a49131c1b415fd
Dennis-83 <dodo_ez@hotmail.com>
2024-11-27 17:33:24 +0100

    Added aardbeiden counter + reset button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-
 import './App.css'
+import React from "react";
 
 function App() {
-
-  return (
-    <>
-        <h1>Fruitmand bezorgservice</h1>
-    </>
-  )
+    const [counter, setCounter] = React.useState(0);
+    return (
+        <>
+            <h1>Fruitmand bezorgservice</h1>
+            <div className="fruit-counter-card"> 🍓 Aardbeien
+                <button disabled= {counter <= 0}
+                        type="button" onClick={() => setCounter(counter - 1)}>
+                    -
+                </button>
+                {counter}
+                <button type="button" onClick={() => setCounter(counter + 1)}>
+                    +
+                </button>
+            </div>
+            <button type="button" onClick={() => setCounter(0)}>Reset</button>
+        </>
+    )
 }
 
 export default App
---
+++ src/assets/components/Button/Button.jsx
@@ -0,0 +1,14 @@
+// @flow
+import * as React from 'react';
+
+type Props = {
+
+};
+
+export function Button(props: Props) {
+    return (
+        <div>
+
+        </div>
+    );
+};
